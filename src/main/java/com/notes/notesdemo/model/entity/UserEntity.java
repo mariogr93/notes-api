0bb1ecb2ae6e63e147ce5c61653bd0cee4fa9fa9
Mario Guadalupe <mariogr93@gmail.com>
2023-05-02 22:11:28 -0400

    created user entity, added app yml properties files

---
+++ src/main/java/com/notes/notesdemo/model/entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.notes.notesdemo.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserEntity(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
