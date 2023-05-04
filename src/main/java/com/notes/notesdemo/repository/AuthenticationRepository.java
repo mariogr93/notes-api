be9c33b4edd5ff7d5cb19172fbfb30d14c533370
Mario Guadalupe <mariogr93@gmail.com>
2023-05-03 20:45:56 -0400

    add existsbyemail and findbyemail

--- src/main/java/com/notes/notesdemo/repository/AuthenticationRepository.java
+++ src/main/java/com/notes/notesdemo/repository/AuthenticationRepository.java
@@ -4,6 +4,11 @@ import com.notes.notesdemo.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthenticationRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
