5cfaf7757aa8fc804cd361f4b16d485429dbb33b
Mario Guadalupe <mariogr93@gmail.com>
2023-05-03 20:46:44 -0400

    add DTOs validation to endpoints

--- src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
+++ src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
@@ -2,10 +2,14 @@ package com.notes.notesdemo.controller;
 
 
 import com.notes.notesdemo.model.entity.UserEntity;
+import com.notes.notesdemo.model.request.UserLoginDTO;
+import com.notes.notesdemo.model.request.UserRegisterDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,12 +18,12 @@ public class AuthenticationController {
 
 
     @PostMapping("/register")
-    public String registrer(@RequestBody UserEntity user) {
+    public String registrer(@Valid @RequestBody UserRegisterDTO user) {
         return "registered";
     }
 
-    @PostMapping("/auth")
-    public String login(@RequestBody UserEntity user) {
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody UserLoginDTO user) {
         return "authenticated";
     }
 
