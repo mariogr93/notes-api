215ccd3382c0444d5337f25712c84f6f3f276633
Mario Guadalupe <mariogr93@gmail.com>
2023-05-03 20:36:22 -0400

    modified authenticationService

--- src/main/java/com/notes/notesdemo/service/AuthenticationService.java
+++ src/main/java/com/notes/notesdemo/service/AuthenticationService.java
@@ -1,8 +1,10 @@
 package com.notes.notesdemo.service;
 
 import com.notes.notesdemo.model.entity.UserEntity;
+import com.notes.notesdemo.model.request.UserRegisterDTO;
 import com.notes.notesdemo.repository.AuthenticationRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +13,13 @@ public class AuthenticationService {
 
     private AuthenticationRepository authenticationRepository;
 
-    public UserEntity registerUser(UserEntity user) {
+    public UserEntity registerUser(UserRegisterDTO userRegisterDTO) {
+        UserEntity user = userRegisterDTO.createUserEntity();
         return this.authenticationRepository.save(user);
+    }
+
+    public UserEntity loginUser(UserEntity user) {
+        return this.authenticationRepository.findByEmail(user.getEmail()).get();
     }
 
 }
