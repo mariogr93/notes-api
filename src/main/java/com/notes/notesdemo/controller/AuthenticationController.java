834c0c4db41d1c85804b99782b9a7c549d1ceb03
Mario Guadalupe <mariogr93@gmail.com>
2023-05-04 21:27:07 -0400

    add general response to request

--- src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
+++ src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
@@ -4,14 +4,17 @@ package com.notes.notesdemo.controller;
 import com.notes.notesdemo.model.entity.UserEntity;
 import com.notes.notesdemo.model.request.UserLoginDTO;
 import com.notes.notesdemo.model.request.UserRegisterDTO;
+import com.notes.notesdemo.model.response.GeneralResponse;
 import com.notes.notesdemo.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -21,13 +24,13 @@ public class AuthenticationController {
     private AuthenticationService authService;
 
     @PostMapping("/register")
-    public UserEntity registrer(@RequestBody @Valid UserRegisterDTO user) {
-        return this.authService.registerUser(user);
+    public GeneralResponse registrer(@RequestBody @Valid UserRegisterDTO user) {
+        return GeneralResponse.builder().data(Map.of("user", this.authService.registerUser(user))).build();
     }
 
     @PostMapping("/login")
-    public UserEntity login(@RequestBody @Valid UserLoginDTO user) {
-        return this.authService.loginUser(user);
+    public GeneralResponse login(@RequestBody @Valid UserLoginDTO user) {
+        return GeneralResponse.builder().data(Map.of("user", this.authService.loginUser(user))).build();
     }
 
 }
