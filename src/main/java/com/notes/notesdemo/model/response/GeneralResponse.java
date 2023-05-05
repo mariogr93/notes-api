ef600b92ba29d67ec234436a0d030f3a5368397c
Mario Guadalupe <mariogr93@gmail.com>
2023-05-04 21:26:40 -0400

    create general response class

---
+++ src/main/java/com/notes/notesdemo/model/response/GeneralResponse.java
@@ -0,0 +1,32 @@
+package com.notes.notesdemo.model.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeneralResponse {
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+    protected List<String> error;
+
+}
