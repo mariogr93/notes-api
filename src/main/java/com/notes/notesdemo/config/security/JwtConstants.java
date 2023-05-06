644589352277cd390c8aa75578fce79e05e819c3
Mario Guadalupe <mariogr93@gmail.com>
2023-05-06 11:04:47 -0400

    create jwt constants

---
+++ src/main/java/com/notes/notesdemo/config/security/JwtConstants.java
@@ -0,0 +1,8 @@
+package com.notes.notesdemo.config.security;
+
+public class JwtConstants {
+
+    public static final int JWT_EXPIRATION = 700000;
+    public static final String JWT_SECRET = "404D635166546A576E5A713474377721"; //ponerle un nombre secreto, complejo y largo  https://www.allkeysgenerator.com/Random/Security-Encryption-Key-Generator.aspx
+
+}
