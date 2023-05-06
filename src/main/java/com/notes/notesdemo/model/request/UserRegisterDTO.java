0e36d1fb35139e44ade574905d06cb01de0d89f7
Mario Guadalupe <mariogr93@gmail.com>
2023-05-05 22:40:20 -0400

    add role

--- src/main/java/com/notes/notesdemo/model/request/UserRegisterDTO.java
+++ src/main/java/com/notes/notesdemo/model/request/UserRegisterDTO.java
@@ -2,10 +2,12 @@ package com.notes.notesdemo.model.request;
 
 
 import com.notes.notesdemo.model.entity.UserEntity;
+import com.notes.notesdemo.model.enums.Role;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -20,8 +22,10 @@ public class UserRegisterDTO {
     private String email;
     @NotBlank(message = "The  password is required.")
     private String password;
+    @NotNull(message = "The  role is required.")
+    private Role role;
 
     public UserEntity createUserEntity() {
-        return new UserEntity(getFirstName(), getLastName(), getEmail(), getPassword());
+        return new UserEntity(getFirstName(), getLastName(), getEmail(), getPassword(), getRole());
     }
 }
