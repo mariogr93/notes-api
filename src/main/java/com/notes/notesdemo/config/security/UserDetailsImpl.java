3382771ec32647c82cc6656ab46d7d323faa8a2c
Mario Guadalupe <mariogr93@gmail.com>
2023-05-05 22:41:53 -0400

    create UserDetailsImpl class and UserDetailsServiceImpl service

---
+++ src/main/java/com/notes/notesdemo/config/security/UserDetailsImpl.java
@@ -0,0 +1,68 @@
+package com.notes.notesdemo.config.security;
+
+import com.notes.notesdemo.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.util.Collection;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+
+    @Override
+    public String toString() {
+        return "UserDetailsImpl{" +
+                "email='" + getEmail() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", role=" + getRole() +
+                '}';
+    }
+}
---
+++ src/main/java/com/notes/notesdemo/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.notes.notesdemo.config.security;
+
+
+import com.notes.notesdemo.repository.AuthenticationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AuthenticationRepository authService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.authService.findByEmail(username).map(user -> new UserDetailsImpl(user.getFirstName(),
+                        user.getLastName(),
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getRole()))
+                .orElseThrow(() -> new UsernameNotFoundException("Username Not Found Exception"));
+    }
+}
