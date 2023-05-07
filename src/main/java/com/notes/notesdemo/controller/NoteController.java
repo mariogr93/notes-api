a6e5de2569283b70ae269ff58b21fe10d20920b8
Mario Guadalupe <mariogr93@gmail.com>
2023-05-07 15:10:00 -0400

    mapped response entity returns

--- src/main/java/com/notes/notesdemo/controller/NoteController.java
+++ src/main/java/com/notes/notesdemo/controller/NoteController.java
@@ -2,21 +2,49 @@ package com.notes.notesdemo.controller;
 
 
 import com.notes.notesdemo.model.request.NoteDTO;
+import com.notes.notesdemo.model.response.GeneralResponse;
+import com.notes.notesdemo.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
 
 @RestController
 @RequestMapping("/api/v1/notes")
+@RequiredArgsConstructor
 public class NoteController {
 
-    @GetMapping
-    public String getNotesByUserId(){
-        return "notes";
+    private final NoteService noteService;
+
+    @GetMapping(name = "/user/{userId}")
+    public ResponseEntity<GeneralResponse> getNotesByUserId(@PathVariable Integer userId){
+
+        return ResponseEntity.ok(GeneralResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .message("Registered notes")
+                .data(Map.of("notes", this.noteService.getAllUserNotes(userId)))
+                .status(HttpStatus.FOUND)
+                .statusCode(HttpStatus.FOUND.value())
+                .build()
+        );
     }
 
     @PostMapping
-    public String saveNote(@RequestBody @Valid NoteDTO note) {
-        return "saved";
+    public ResponseEntity<GeneralResponse>  saveNote(@RequestBody @Valid NoteDTO note) {
+
+        return ResponseEntity.ok(GeneralResponse.builder()
+                .timeStamp(now())
+                .data(Map.of("note", this.noteService.saveNote(note)))
+                .message("Note saved")
+                .statusCode(200)
+                .status(HttpStatus.CREATED)
+                .build()
+        );
     }
 }
