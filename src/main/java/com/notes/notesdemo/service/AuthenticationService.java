dcf3bfef40f550f76a1e4cbf252e375f65a86cc8
Mario Guadalupe <mariogr93@gmail.com>
2023-05-06 22:46:05 -0400

    adapt AuthenticationService to use new JWT authentication

--- src/main/java/com/notes/notesdemo/service/AuthenticationService.java
+++ src/main/java/com/notes/notesdemo/service/AuthenticationService.java
@@ -1,26 +1,56 @@
 package com.notes.notesdemo.service;
 
+import com.notes.notesdemo.config.security.JwtService;
+import com.notes.notesdemo.config.security.UserDetailsImpl;
 import com.notes.notesdemo.model.entity.UserEntity;
 import com.notes.notesdemo.model.request.UserLoginDTO;
 import com.notes.notesdemo.model.request.UserRegisterDTO;
+import com.notes.notesdemo.model.response.UserLoginResponse;
 import com.notes.notesdemo.repository.AuthenticationRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final AuthenticationRepository authenticationRepository;
+    private final AuthenticationRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     public UserEntity registerUser(UserRegisterDTO userRegisterDTO) {
-        UserEntity user = userRegisterDTO.createUserEntity();
-        return this.authenticationRepository.save(user);
+        if(this.authRepository.existsByEmail(userRegisterDTO.getEmail())){
+            throw new IllegalStateException("Email already registered");
+        }
+
+        UserEntity newUser = userRegisterDTO.createUserEntity();
+        newUser.setPassword(this.passwordEncoder.encode(userRegisterDTO.getPassword()));
+        UserEntity registeredUser = this.authRepository.save(newUser);
+        registeredUser.setPassword("");
+        return  registeredUser;
     }
 
-    public UserEntity loginUser(UserLoginDTO user) {
-        return this.authenticationRepository.findByEmail(user.getEmail()).get();
+    public UserLoginResponse loginUser(UserLoginDTO userLoginDTO) {
+
+        Authentication authentication = this.authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword()));
+        UserDetailsImpl authenticatedUser = (UserDetailsImpl) authentication.getPrincipal();
+        UserEntity user = this.authRepository.findByEmail(authenticatedUser.getUsername()).get();
+        String token = this.jwtService.generateToken(authenticatedUser);
+
+        return UserLoginResponse.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .token(token)
+                .role(user.getRole())
+                .build();
     }
 
 }
