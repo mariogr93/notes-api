07226e68ffdbe3cdb01649a999063aaf2b78a0e9
Mario Guadalupe <mariogr93@gmail.com>
2023-05-06 22:46:47 -0400

    modify controler to respond with ResponseEntity and more complete response object

--- src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
+++ src/main/java/com/notes/notesdemo/controller/AuthenticationController.java
@@ -5,8 +5,10 @@ import com.notes.notesdemo.model.entity.UserEntity;
 import com.notes.notesdemo.model.request.UserLoginDTO;
 import com.notes.notesdemo.model.request.UserRegisterDTO;
 import com.notes.notesdemo.model.response.GeneralResponse;
+import com.notes.notesdemo.model.response.UserLoginResponse;
 import com.notes.notesdemo.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
@@ -24,13 +27,29 @@ public class AuthenticationController {
     private final AuthenticationService authService;
 
     @PostMapping("/register")
-    public GeneralResponse registrer(@RequestBody @Valid UserRegisterDTO user) {
-        return GeneralResponse.builder().data(Map.of("user", this.authService.registerUser(user))).build();
+    public ResponseEntity<GeneralResponse> registrer(@RequestBody @Valid UserRegisterDTO user) {
+        return ResponseEntity.ok(GeneralResponse.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .message("User registered succesfuly!")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
+                        .data(Map.of("user", this.authService.registerUser(user)))
+                        .build());
     }
 
     @PostMapping("/login")
-    public GeneralResponse login(@RequestBody @Valid UserLoginDTO user) {
-        return GeneralResponse.builder().data(Map.of("user", this.authService.loginUser(user))).build();
+    public ResponseEntity<GeneralResponse> login(@RequestBody @Valid UserLoginDTO user) {
+        UserLoginResponse authenticatedUser = this.authService.loginUser(user);
+
+        return ResponseEntity.ok(
+                GeneralResponse.builder()
+                        .message("User logged in")
+                        .data(Map.of("user", authenticatedUser))
+                        .statusCode(HttpStatus.FOUND.value())
+                        .status(HttpStatus.FOUND)
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 
 }
